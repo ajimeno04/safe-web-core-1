162ccf2f2e795e1eb1ff06f2872a2ae91781dc63
datradito <datradito@gmail.com>
2022-12-15 11:07:34 -0300

    feat: fix migration config

--- src/services/ls-migration/config.ts
+++ src/services/ls-migration/config.ts
@@ -1,3 +1,3 @@
-export const IFRAME_HOST = 'https://staging.multisig.bobabeam.boba.network'
+export const IFRAME_HOST = 'https://staging-safe.astar.network'
 export const IFRAME_PATH = '/migrate.html'
 export const MIGRATION_KEY = 'migrationFinished'
